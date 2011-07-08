83ac10d3f377de753a2b980c79addef3a2fe7c7a
VLO <VLO@bab10101-e421-0410-a517-8ce0973de3ef>
2011-07-08 16:07:06 +0000

    rename package folderversion
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/applications/plu/trunk/plugin-plu@32176 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/plu/business/folderversion/IFolderVersionDAO.java
+++ src/java/fr/paris/lutece/plugins/plu/business/folderversion/IFolderVersionDAO.java
@@ -31,7 +31,7 @@
  *
  * License 1.0
  */
-package fr.paris.lutece.plugins.plu.business.folderVersion;
+package fr.paris.lutece.plugins.plu.business.folderversion;
 
 import fr.paris.lutece.plugins.plu.business.folder.Folder;
 import fr.paris.lutece.plugins.plu.business.version.Version;
