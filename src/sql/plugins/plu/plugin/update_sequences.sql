a2dfd7e1bf86aff9dd33cde559cab588c113c2a8
ABA <ABA@bab10101-e421-0410-a517-8ce0973de3ef>
2011-09-29 15:09:22 +0000

    Add script for updating sequences after "reprise"
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/applications/plu/trunk/plugin-plu@35275 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/plu/plugin/update_sequences.sql
@@ -0,0 +1,7 @@
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_histo ) FROM plu_historique ) +1 WHERE sequence_name="plu_historique_id";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_iso ) FROM plu_iso ) +1 WHERE sequence_name="plu_iso_id";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_plu ) FROM plu_plu ) +1 WHERE sequence_name="plu_id";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_dossier ) FROM plu_dossier ) +1 WHERE sequence_name="plu_dossier_id";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_dossier_version ) FROM plu_dossier_version_atome ) +1 WHERE sequence_name="plu_dossier_version_atome_id";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_version ) FROM plu_version_atome ) +1 WHERE sequence_name="plu_version_atome_sequence";
+UPDATE `plu_sequences` SET next_val= ( SELECT max( id_version ) FROM plu_version_atome ) +1 WHERE sequence_name="plu_version_atome_id";
