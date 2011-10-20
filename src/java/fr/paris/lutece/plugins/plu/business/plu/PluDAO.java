19fcf1e55429e90bdd44c9c20888bb4add1da49b
ABA <ABA@bab10101-e421-0410-a517-8ce0973de3ef>
2011-10-20 11:06:52 +0000

    Correct potential sql injection
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/applications/plu/trunk/plugin-plu@35824 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/plu/business/plu/PluDAO.java
+++ src/java/fr/paris/lutece/plugins/plu/business/plu/PluDAO.java
@@ -33,6 +33,12 @@
  */
 package fr.paris.lutece.plugins.plu.business.plu;
 
+import fr.paris.lutece.plugins.plu.services.PluPlugin;
+import fr.paris.lutece.portal.service.jpa.JPALuteceDAO;
+import fr.paris.lutece.portal.service.util.AppException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,9 +46,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-
-import fr.paris.lutece.plugins.plu.services.PluPlugin;
-import fr.paris.lutece.portal.service.jpa.JPALuteceDAO;
 
 
 /**
@@ -53,7 +56,8 @@ public class PluDAO extends JPALuteceDAO<Integer, Plu> implements IPluDAO
 {
     private static final String SQL_QUERY_SELECT_PLU_WORK = "SELECT p FROM Plu p WHERE p.da IS NULL";
     private static final String SQL_QUERY_SELECT_PLU_APPLIED = "SELECT p FROM Plu p WHERE p.id = ( SELECT MAX(p.id) - 1 FROM Plu p )";
-    private static final String SQL_QUERY_SELECT_PLU_SEARCH_BY_DATE_APPLICATION = "SELECT p FROM Plu p WHERE p.da > date1 AND p.da < date2";
+    private static final String SQL_QUERY_SELECT_PLU_SEARCH_BY_DATE_APPLICATION = "SELECT p FROM Plu p WHERE p.da > ? AND p.da < ?";
+    private static final SimpleDateFormat SDF = new SimpleDateFormat( "yyyy-MM-dd" );
 
     /**
      * Returns the list of plu with find with filters
@@ -65,11 +69,19 @@ public class PluDAO extends JPALuteceDAO<Integer, Plu> implements IPluDAO
     {
 
         String query = SQL_QUERY_SELECT_PLU_SEARCH_BY_DATE_APPLICATION;
-        query = query.replace( "date1", "'" + dateApplicationDebut + "'" );
-        query = query.replace( "date2", "'" + dateApplicationFin + "'" );
         TypedQuery<Plu> q = this.getEM( ).createQuery( query, Plu.class );
 
         List<Plu> listPlu;
+
+        try
+        {
+            q.setParameter( 1, SDF.parse( dateApplicationDebut ) );
+            q.setParameter( 2, SDF.parse( dateApplicationFin ) );
+        }
+        catch ( ParseException e1 )
+        {
+            throw new AppException( "Erreur lors de la récupération de la date d'application.", e1 );
+        }
 
         try
         {
