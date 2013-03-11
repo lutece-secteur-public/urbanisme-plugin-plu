6dabddf444a3851fff4d13674222c7e9fc9f9cd0
ABA <ABA@bab10101-e421-0410-a517-8ce0973de3ef>
2013-03-11 14:36:35 +0000

    Mantis 162
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/applications/plu/trunk/plugin-plu@47305 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/plu/plugin/update_plu_1.0.3-1.0.4.sql
@@ -0,0 +1,3 @@
+/* MANTIS 162 - Atome en double dans le dossier "Logement social et protection du commerce et de l'artisanat" 
+ * du PLU 11 */
+DELETE from plu_dossier_version_atome where plu_dossier_version_atome.id_dossier_version=6878;
