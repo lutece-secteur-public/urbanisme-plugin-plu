6f2b9ce8c9b06afcfd01c5458fd7f6b0cb4b48f6
ABA <ABA@bab10101-e421-0410-a517-8ce0973de3ef>
2013-03-14 10:36:31 +0000

    Correction apostrophe
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/applications/plu/trunk/plugin-plu@47392 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/plu/plugin/update_plu_1.0.3-1.0.4.sql
+++ src/sql/plugins/plu/plugin/update_plu_1.0.3-1.0.4.sql
@@ -1,7 +1,7 @@
-/* MANTIS 162 - Atome en double dans le dossier "Logement social et protection du commerce et de l'artisanat" 
+/* MANTIS 162 - Atome en double dans le dossier "Logement social et protection du commerce et de lartisanat" 
  * du PLU 11 */
 DELETE from plu_dossier_version_atome where plu_dossier_version_atome.id_dossier_version=6878;
 
-/* MANTIS 166 - probleme d'encodage dans les pages html specifiques */
+/* MANTIS 166 - probleme dencodage dans les pages html specifiques */
 ALTER TABLE `plu_dossier` CHANGE `html_specifique_c` `html_specifique_c` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `plu_dossier` CHANGE `html_specifique_i` `html_specifique_i` MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
